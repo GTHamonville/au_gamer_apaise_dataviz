d636af8899cd11983e957037a418ecc5aa49f510
Guillaume Tardif d'Hammonville <guillaume.tardif@lecole-ldlc.com>
2018-02-01 13:30:04 +0100

    fix new images add preload

--- scripts/button_bg_change.js
+++ scripts/button_bg_change.js
@@ -5,24 +5,28 @@ var current_game = "League of Legends";
 $('#lol').click(function () {
     $('#joyplot').css('background-image', 'url(style/img/lolv3.png)');
     current_game = "League of Legends";
+    $('#vert_img').attr('src', 'style/img/vert_pos1.jpg');
     reload();
 });
 
 $('#bf3').click(function () {
     $('#joyplot').css('background-image', 'url(style/img/bf3v3.png)');
     current_game = "Battlefield";
+    $('#vert_img').attr('src', 'style/img/vert_pos2.jpg');
     reload();
 });
 
 $('#swtor').click(function () {
     $('#joyplot').css('background-image', 'url(style/img/swotor3.png)');
     current_game = "Star Wars";
+    $('#vert_img').attr('src', 'style/img/vert_pos3.jpg');
     reload();
 });
 
 $('#sky').click(function () {
     $('#joyplot').css('background-image', 'url(style/img/skyrim3.png)');
     current_game = "The Elder Scrolls";
+    $('#vert_img').attr('src', 'style/img/vert_pos4.jpg');
     reload();
 });
 
